5165276855d266deb710bed2b4da3ee87f0aef7b
Isaac Alves Leal de Oliveira <isaacalves2406@gmail.com>
2024-08-24 10:33:18 -0300

    Atualizado

--- src/RespositoryEDA/src/Arvore/main.java
+++ src/RespositoryEDA/src/Arvore/main.java
@@ -10,12 +10,5 @@ public class main {
         teste.inserir(28);
         teste.inserir(4);
         printer.printTree(teste);
-        teste.inserirBalaceado(19);
-        teste.inserirBalaceado(5);
-        teste.inserirBalaceado(21);
-        teste.inserirBalaceado(24);
-        teste.inserirBalaceado(28);
-        teste.inserirBalaceado(4);
-        printer.printTree(teste);
     }
 }
