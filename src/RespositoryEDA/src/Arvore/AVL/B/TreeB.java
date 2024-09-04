a6e5fe0387fdbf0213da1fcf51c7239403f24c67
isaacoliveeira <isaacalves2406@gmail.com>
2024-09-04 11:35:53 -0300

    tree b

--- src/RespositoryEDA/src/Arvore/AVL/B/TreeB.java
+++ src/RespositoryEDA/src/Arvore/AVL/B/TreeB.java
@@ -1,19 +1,34 @@
+package src.Arvore.AVL.B;
+
 public class TreeB {
 
     private No raiz;
+    private int ordem;
 
-    public TreeB() { this.raiz = null; }
+    public TreeB(int ordem) { this.raiz = null; this.ordem = ordem; }
 
     public No getRaiz() { return raiz;}
 
-    public void inserir(int[] elementos) {
+    public void inserir(int valor) {
         if (raiz == null) {
-            raiz = new No();
-            raiz.inserirElemento(elementos[0]);
+            raiz = new No(ordem, true);
+            raiz.getElemento()[0] = valor;
+        } else {
 
         }
     }
+
     public void inserirElemento(No no, int elementos) {
 
+    }
+
+    public int numeroElemento(No no) {
+        int count = 0;
+        for (int i = 0; i < no.getElemento().length; i++) {
+            if (no.getElemento()[i] != 0) {
+                count++;
+            }
+        }
+        return count;
     }
 }
